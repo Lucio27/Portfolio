d8705620040278d8fd78184a449f05b59a777413
Lucio27 <rodrigolucio85@gmail.com>
2022-01-27 10:22:17 -0600

    Agregue Carpeta Dacodes

--- Algorithms/Two_Number_Sum.py
+++ Algorithms/Two_Number_Sum.py
@@ -1,14 +1,3 @@
-# def twoNumberSum(array, targetSum):
-#     # Write your code here.
-# 	new_list = []
-# 	# for num in array:
-# 	# 	for i in range(len(array)):
-# 	# 		if i is not array.index(num):
-#     #             result = num + array[i]
-#     #             if result == targetSum:
-#     #                 new_list.append(num)
-#     #                 new_list.append(array[i])
-#     # return new_list
 def twoNumberSum(array,targetSum):
     new_list = []
     for num in array:
