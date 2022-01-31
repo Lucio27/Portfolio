edb44190068af28c8a3174f7249dd447dba39d17
Lucio27 <rodrigolucio85@gmail.com>
2022-01-30 19:20:06 -0600

    Agregue ultimos cambios

--- DaCodes/DaCodes.py
+++ DaCodes/DaCodes.py
@@ -1,1 +1,16 @@
-print('test')
+from collections import Counter
+
+def solution(array):
+    return max(tuple(Counter(array).values()))
+
+Given_A = ["Cardiology","Orthopaedics","Neurology","Cardiology","Orthopaedics","Cardiology"]
+# Given_A = ["Oncology","Gynaecology","Orthopaedics","Oncology","Gynaecology","Orthopaedics"]
+# Given_A = ["Neutology","Cardiology","Oncology"]
+print(solution(Given_A))
+
+
+
+    
+
+
+
